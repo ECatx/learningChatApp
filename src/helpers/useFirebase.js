7615b57f50c9f9e0ca04fb2a16ca8a64bbc8f0dd
Eric Castellanos <eric@ericcastellanos.com>
2021-04-19 23:27:02 -0500

    Firebase connection

--- src/helpers/useFirebase.js
+++ src/helpers/useFirebase.js
@@ -1,12 +1,14 @@
+import {ref, onUnmounted} from 'vue'
 import firebase from 'firebase'
 import {useAuth} from '@vueuse/firebase'
 import 'firebase/auth'
 import {firebaseConfig} from '../config/firebase'
+import 'firebase/firestore'
 
 firebase.initializeApp(firebaseConfig)
-const {auth} = firebase
+const {auth,firestore} = firebase
 const {GoogleAuthProvider} = auth
-
+const db = firestore()
 const {isAuthenticated, user} = useAuth()
 
 export const authentication = () => {
@@ -16,5 +18,26 @@ export const authentication = () => {
 }
 
 export const database = () => {
+    const messages = ref([])
+
+    const messageCollection = db.collection('messages')
+    const messageQuery = messageCollection.orderBy('createdAt','desc').limit(100)
+
+    const unsubscribe =  messageQuery.onSnapshot(s=>{
+        messages.value = s.docs.map(doc=>({id:s.id, ...doc.data})).reverse()
+    })
+    onUnmounted(unsubscribe)
+    const sendMessage = text => {
+        if(!isAuthenticated.value) return 
+        const {photoURL, uid, displayName} = user.value
+        messageCollection.add({
+            userName: displayName,
+            userPhotoURL: photoURL,
+            text: text,
+            createdAt: firebase.firestore.FieldValue.serverTimestamp(),    
+        })
+    }
+    return { messages, sendMessage }
+
 
 }
