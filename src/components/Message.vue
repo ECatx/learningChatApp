75e97dc059bfa01d31f2ef9a077792b31cf4f063
Eric Castellanos <eric@ericcastellanos.com>
2021-04-19 22:38:22 -0500

    Layout for messages

---
+++ src/components/Message.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="container mx-auto flex bg-blue-900 my-3 rounded-full">
+        <div>
+           <mdi:cat class="bg-white rounded-full h-14 w-14"/> 
+        </div>
+        <div class="flex flex-grow">
+            <div class="flex flex-grow mx-5 justify-between items-center text-lightBlue-100 text-2xl">
+                <p>Message Example</p> 
+                <p class="font-light text-xs">By Cat</p>
+            </div>
+        </div>
+    </div>
+</template>
